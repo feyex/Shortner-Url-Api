9263fe5f4dd34ddb6ce9b04ac499abba5f4ba6b3
feyisewa <feyisewaawotedu@gmail.com>
2022-05-20 21:12:32 +0200

    e2e test added

--- test.js
+++ test.js
@@ -74,7 +74,7 @@ describe("Shortner Api Url test", () => {
             .end((err, res) => {
                 res.status.should.equal(400);
                 console.log(res)
-                res.text.should.equall('Short Code Url Value Cannot be less than four in length');
+                res.body.message.should.equall('Short Code Url Value Cannot be less than four in length');
                 done();
             });
     });
